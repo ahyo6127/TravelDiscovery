90bdd5cbdbb696623d5ba4cee6dc94c2e5ca8e72
Travis Lu <ahyo6127@gmail.com>
2023-02-21 15:46:13 +0800

    Change file name

---
+++ TravelDiscovery/TravelDiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDiscoveryApp.swift
+//  TravelDiscovery
+//
+//  Created by Travis on 2023/2/19.
+//
+
+import SwiftUI
+
+@main
+struct TravelDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
