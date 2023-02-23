5632c5a87b6b9e887d1720c157b0fe625319b198
Travis Lu <ahyo6127@gmail.com>
2023-02-23 23:44:54 +0800

    Add 3 views
    PopularDesnationsView()
    PopularRestaurantsView()
    TrendingCreatorsView()

--- TravelDiscovery/ContentView.swift
+++ TravelDiscovery/ContentView.swift
@@ -15,6 +15,12 @@ struct ContentView: View {
                 
                 DiscoverCatagoriesView()
                 
+                PopularDesnationsView()
+                
+                PopularRestaurantsView()
+                
+                TrendingCreatorsView()
+                
             }.navigationTitle("Discover")
         }
         //            Image(systemName: "globe")
@@ -24,6 +30,90 @@ struct ContentView: View {
         //                .padding()
         //                .navigationBarTitle("Inline", displayMode: .inline)
 
+    }
+}
+
+struct PopularDesnationsView: View {
+    var body: some View{
+        VStack {
+            HStack {
+                Text("Popular destinations")
+                    .font(.system(size: 12, weight: .semibold))
+                Spacer()
+                Text("See all")
+                    .font(.system(size: 12, weight: .semibold))
+            }.padding(.horizontal)
+        }.padding(.top)
+        
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 8.0) {
+                ForEach( 0..<5, id: \.self ) { num in
+                    Spacer()
+                        .frame(width: 125, height: 150)
+                        .background(Color.gray)
+                        .cornerRadius(5)
+                        .shadow(color: .gray, radius: 4, x: 0.0, y: 0.2)
+                        .padding(.bottom)
+                }
+            }.padding(.horizontal)
+        }
+        
+    }
+}
+
+struct PopularRestaurantsView: View {
+    var body: some View{
+        VStack {
+            HStack {
+                Text("Popular place to eat")
+                    .font(.system(size: 12, weight: .semibold))
+                Spacer()
+                Text("See all")
+                    .font(.system(size: 12, weight: .semibold))
+            }.padding(.horizontal)
+        }.padding(.top)
+        
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 8.0) {
+                ForEach( 0..<5, id: \.self ) { num in
+                    Spacer()
+                        .frame(width: 200, height: 64)
+                        .background(Color.gray)
+                        .cornerRadius(5)
+                        .shadow(color: .gray, radius: 4, x: 0.0, y: 0.2)
+                        .padding(.bottom)
+                }
+            }.padding(.horizontal)
+        }
+        
+    }
+}
+
+struct TrendingCreatorsView: View {
+    var body: some View{
+        VStack {
+            HStack {
+                Text("Popular Creators")
+                    .font(.system(size: 12, weight: .semibold))
+                Spacer()
+                Text("See all")
+                    .font(.system(size: 12, weight: .semibold))
+            }.padding(.horizontal)
+        }.padding(.top)
+        
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 8.0) {
+                ForEach( 0..<8, id: \.self ) { num in
+                    Spacer()
+                        .frame(width: 50, height: 50)
+                        .background(Color.gray)
+                        .cornerRadius(.infinity)
+                        .shadow(color: .gray, radius: 5, x: 0.0, y: 0.2)
+                        .padding(.bottom)
+                }
+            }.padding(.horizontal)
+        }
+        
     }
 }
 
