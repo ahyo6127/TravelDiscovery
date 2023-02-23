ce8c71fcd7ae1795aef020adef44a7b67f890ae3
Travis Lu <ahyo6127@gmail.com>
2023-02-23 23:25:10 +0800

    Add DiscoverCatagoriesView()

--- TravelDiscovery/ContentView.swift
+++ TravelDiscovery/ContentView.swift
@@ -13,24 +13,8 @@ struct ContentView: View {
             
             ScrollView {
                 
-                ScrollView(.horizontal, showsIndicators: false){
-                    
-                    HStack(spacing: 8) {
-                        ForEach( 0..<5, id: \.self) { num in
-                            VStack(spacing: 4) {
-                                Spacer()
-                                    .frame(width: 50, height: 50)
-                                    .background(Color.gray)
-                                    .cornerRadius(.infinity)
-                                    .shadow(color: .gray ,radius: 5, x: 0.0, y: 0.2)
-                                Text("Art")
-                                    .font(.system(size: 12, weight: .semibold))
-                            }
-                        }
-                    }.padding(.horizontal)
-                    
-                }
-//                .background(Color.blue)
+                DiscoverCatagoriesView()
+                
             }.navigationTitle("Discover")
         }
         //            Image(systemName: "globe")
@@ -46,5 +30,28 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiscoverCatagoriesView: View {
+    var body: some View {
+        
+        ScrollView(.horizontal, showsIndicators: false) {
+            
+            HStack(spacing: 8) {
+                ForEach( 0..<5, id: \.self) { num in
+                    VStack(spacing: 4) {
+                        Spacer()
+                            .frame(width: 50, height: 50)
+                            .background(Color.gray)
+                            .cornerRadius(.infinity)
+                            .shadow(color: .gray ,radius: 5, x: 0.0, y: 0.2)
+                        Text("Art")
+                            .font(.system(size: 12, weight: .semibold))
+                    }
+                }
+            }.padding(.horizontal)
+    //                .background(Color.blue)
+        }
     }
 }
