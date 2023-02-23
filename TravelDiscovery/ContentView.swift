43c8f4461af107006a2f6e4480fd75ac3fe22bd5
Travis Lu <ahyo6127@gmail.com>
2023-02-23 23:22:10 +0800

    NavigationView ScrollViews and Stacking

--- TravelDiscovery/ContentView.swift
+++ TravelDiscovery/ContentView.swift
@@ -9,13 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            
+            ScrollView {
+                
+                ScrollView(.horizontal, showsIndicators: false){
+                    
+                    HStack(spacing: 8) {
+                        ForEach( 0..<5, id: \.self) { num in
+                            VStack(spacing: 4) {
+                                Spacer()
+                                    .frame(width: 50, height: 50)
+                                    .background(Color.gray)
+                                    .cornerRadius(.infinity)
+                                    .shadow(color: .gray ,radius: 5, x: 0.0, y: 0.2)
+                                Text("Art")
+                                    .font(.system(size: 12, weight: .semibold))
+                            }
+                        }
+                    }.padding(.horizontal)
+                    
+                }
+//                .background(Color.blue)
+            }.navigationTitle("Discover")
         }
-        .padding()
+        //            Image(systemName: "globe")
+        //                .imageScale(.large)
+        //                .foregroundColor(.accentColor)
+        //            Text("Hello, world!!!")
+        //                .padding()
+        //                .navigationBarTitle("Inline", displayMode: .inline)
+
     }
 }
 
