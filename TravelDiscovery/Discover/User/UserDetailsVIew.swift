1207d61caed53b12b75846967ea587e57e1f853e
Travis Lu <ahyo6127@gmail.com>
2024-03-02 21:45:29 +0800

    Post Card Layout View
    1. Using Color(#colorLiteral(red: 0.2, green: 0.6, blue: 0.9, alpha: 1))

--- TravelDiscovery/Discover/User/UserDetailsVIew.swift
+++ TravelDiscovery/Discover/User/UserDetailsVIew.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct UserDetailsView: View {
     
@@ -87,11 +88,46 @@ struct UserDetailsView: View {
                 }.font(.system(size: 12, weight: .semibold))
                 
                 ForEach(0..<10, id: \.self) { num in
-                    HStack{
-                        Spacer()
+                    VStack(alignment: .leading) {
+                        KFImage(URL(string: "https://letsbuildthatapp-videos.s3.us-west-2.amazonaws.com/4aff5261-6cea-49ad-a541-cb70b7f13ed3"))
+                            .resizable()
+                            .scaledToFill()
+                            .frame(height: 200)
+                            .clipped()
+                        
+                        HStack {
+                            Image("userMan01")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 34)
+                                .clipShape(Circle())
+                                
+                            VStack(alignment: .leading){
+                                Text("Here is my post title")
+                                    .font(.system(size: 14, weight: .semibold))
+
+                                Text("500k views")
+                                    .font(.system(size: 12, weight: .regular))
+                                    .foregroundStyle(Color(.gray))
+                            }
+                        }.padding(.horizontal, 6)
+                        
+                        HStack {
+                            ForEach(0..<3, id: \.self) { num in
+                                Text("#Traveling")
+                                    .foregroundStyle(Color(#colorLiteral(red: 0.2, green: 0.6, blue: 0.9, alpha: 1)))
+                                    .font(.system(size: 12, weight: .semibold))
+                                    .padding(.horizontal, 12)
+                                    .padding(.vertical, 4)
+                                    .background(Color(#colorLiteral(red: 0.9, green: 0.9, blue: 0.9, alpha: 1)))
+                                    .cornerRadius(20)
+                            }
+                        }.padding(.horizontal, 8)
+                        .padding(.bottom, 10)
+            
                     }
-                    .frame(height: 250)
-                    .background(Color(white: 0.8))
+//                    .frame(height: 250)
+                    .background(Color(white: 1))
                     .cornerRadius(8)
                     .shadow(color: .init(white: 0.8), radius: 5, x: 0, y: 4)
                 }
